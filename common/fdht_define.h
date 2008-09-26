bf9774d5c8be94858524dee2acbe8930cab5f1d8
happyfish100 <happyfish100@5c4485f6-2256-0410-99f7-57f0f8ee7ac6>
2008-09-26 07:57:03 +0000

    add fdht_define.h fdht_proto.c fdht_proto.h

---
+++ common/fdht_define.h
@@ -0,0 +1,29 @@
+/**
+* Copyright (C) 2008 Happy Fish / YuQing
+*
+* FastDFS may be copied only under the terms of the GNU General
+* Public License V3, which may be found in the FastDFS source kit.
+* Please visit the FastDFS Home Page http://www.csource.org/ for more detail.
+**/
+
+//fdht_define.h
+
+#ifndef _FDHT_DEFINE_H_
+#define _FDHT_DEFINE_H_
+
+#include <pthread.h>
+#include "common_define.h"
+
+#define FDHT_SERVER_DEFAULT_PORT  24000
+#define FDHT_MAX_PKG_SIZE         64 * 1024
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
