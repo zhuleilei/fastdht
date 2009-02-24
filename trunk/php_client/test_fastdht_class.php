5df9f1bff1e7508e467a21f4a58773af6156efe7
happyfish100 <happyfish100@5c4485f6-2256-0410-99f7-57f0f8ee7ac6>
2009-02-24 03:52:19 +0000

    php ext

--- trunk/php_client/test_fastdht_class.php
+++ trunk/php_client/test_fastdht_class.php
@@ -5,11 +5,11 @@ $object_id = '';
 $key = 'key';
 $value = 'this is a test';
 
-$fdht = new FastDHT();
-echo $fdht->get($namespace, $object_id, $key);
+$fdht = new FastDHT(false);
+echo $fdht->get($namespace, $object_id, $key) . "\n";
 
 $fdht = new FastDHT();
-echo $fdht->get($namespace, $object_id, $key);
+echo $fdht->get($namespace, $object_id, $key) . "\n";
 if (($result=fastdht_set($namespace, $object_id, $key, $value)) != 0)
 {
 	error_log("fastdht_set fail, errno: $result");
