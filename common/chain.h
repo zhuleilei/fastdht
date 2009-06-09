e2ce7c26edfd2859c9d114b65bb06ec6f5473702
happyfish100 <happyfish100@5c4485f6-2256-0410-99f7-57f0f8ee7ac6>
2009-06-09 04:48:29 +0000

    hash can store value, and pass ChainNode to chain when add node

--- common/chain.h
+++ common/chain.h
@@ -28,21 +28,31 @@ typedef struct
 	ChainNode *tail;
 	FreeDataFunc freeDataFunc;
 	CompareFunc compareFunc;
+	bool is_malloc_node;
 } ChainList;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void chain_init(ChainList *pList, const int type, FreeDataFunc freeDataFunc, \
-		CompareFunc compareFunc);
+#define chain_init(pList, type, freeDataFunc, compareFunc) \
+	chain_init_ex(pList, type, freeDataFunc, compareFunc, true)
+
+#define addNode(pList, data) addNode_ex(pList, data, NULL)
+#define insertNodePrior(pList, data) insertNodePrior_ex(pList, data, NULL)
+#define appendNode(pList, data) appendNode_ex(pList, data, NULL)
+
+void chain_init_ex(ChainList *pList, const int type, FreeDataFunc freeDataFunc,\
+		CompareFunc compareFunc, const bool bMallocNode);
 void chain_destroy(ChainList *pList);
 
 int chain_count(ChainList *pList);
 
-int addNode(ChainList *pList, void *data);
-void freeChainNode(ChainList *pList, ChainNode *pChainNode);
+int addNode_ex(ChainList *pList, void *data, ChainNode *pNode);
+int insertNodePrior_ex(ChainList *pList, void *data, ChainNode *pNode);
+int appendNode_ex(ChainList *pList, void *data, ChainNode *pNode);
 
+void freeChainNode(ChainList *pList, ChainNode *pChainNode);
 void deleteNodeEx(ChainList *pList, ChainNode *pPreviousNode, \
 		ChainNode *pDeletedNode);
 void deleteToNodePrevious(ChainList *pList, ChainNode *pPreviousNode, \
@@ -50,9 +60,6 @@ void deleteToNodePrevious(ChainList *pList, ChainNode *pPreviousNode, \
 int deleteOne(ChainList *pList, void *data);
 int deleteAll(ChainList *pList, void *data);
 void *chain_pop_head(ChainList *pList);
-
-int insertNodePrior(ChainList *pList, void *data);
-int appendNode(ChainList *pList, void *data);
 
 #ifdef __cplusplus
 }
