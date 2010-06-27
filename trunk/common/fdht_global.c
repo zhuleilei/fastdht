40773be46f6d088b1c47deca96c80652b72527f7
happyfish100 <happyfish100@5c4485f6-2256-0410-99f7-57f0f8ee7ac6>
2010-06-27 12:49:55 +0000

    log_init set log to cache to false (no cache)

--- trunk/common/fdht_global.c
+++ trunk/common/fdht_global.c
@@ -13,5 +13,5 @@
 
 int g_fdht_network_timeout = DEFAULT_NETWORK_TIMEOUT;
 char g_fdht_base_path[MAX_PATH_SIZE] = {'/', 't', 'm', 'p', '\0'};
-Version g_fdht_version = {1, 13};
+Version g_fdht_version = {1, 14};
 
